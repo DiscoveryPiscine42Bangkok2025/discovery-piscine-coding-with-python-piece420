b32ec10ce6cc55d95174991d334bc709f1cebd0a
piece420 <65050420@kmitl.ac.th>
2025-09-17 12:33:27 +0700

    Add files via upload

---
+++ cell04/ex01/age.py
@@ -0,0 +1,6 @@
+user_age = int(input("Please tell me your age: "))
+print(f"You are currently {user_age} years old.")
+i = 10
+while i <= 30:
+    print(f"In {i} years, you'll be {user_age + i} years old.")
+    i += 10
---
+++ cell04/ex03/float.py
@@ -0,0 +1,5 @@
+user_number = float(input("Give me a number: "))
+if user_number % 1 == 0:
+    print("This number is an integer.")
+else:
+    print("This number is a decimal.")
